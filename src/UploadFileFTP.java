f07f51b9128b6474b6a7693496bffe8f111dd2fb
T.Ron <ronald2000fr@gmail.com>
2023-07-19 23:45:49 +0200

    fix update data

--- src/UploadFileFTP.java
+++ src/UploadFileFTP.java
@@ -15,46 +15,53 @@ public class UploadFileFTP {
             databaseSourceFile = args[0];
 
         JSONArray listObject = DataBase.getInfoConnexion(databaseSourceFile);
+        try {
+            session = jsch.getSession("u85460117-upload", "home631778145.1and1-data.host", 22);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword("FyK6cIAgpeNOSbEdfrpC*");
+            session.connect();
+        ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
+        sftpChannel.connect();
         if (listObject != null) {
             for (Object o : listObject) {
                 JSONObject list = (JSONObject) o;
-
                 if (list.get("reception").equals("1") && list.get("active").equals("1")) {
-                    try {
-                        session = jsch.getSession("u85460117-upload", "home631778145.1and1-data.host", 22);
-                        session.setConfig("StrictHostKeyChecking", "no");
-                        session.setPassword("FyK6cIAgpeNOSbEdfrpC*");
-                        session.connect();
 
-                        Channel channel = session.openChannel("sftp");
-                        channel.connect();
-                        ChannelSftp sftpChannel = (ChannelSftp) channel;
-                        File dir = new File((String) list.get("path"));
-                        sftpChannel.cd((String) list.get("folderftp"));
+                    // Check if folder exists
+                    Vector<ChannelSftp.LsEntry> entries = sftpChannel.ls("/");
 
-                        Vector<ChannelSftp.LsEntry> listFiles = sftpChannel.ls("*.zip");
-                        for (ChannelSftp.LsEntry entry : listFiles) {
-                            if (!(dir).isDirectory())
-                                dir.mkdir();
-                            System.out.println(dir.getAbsolutePath() + "\\" + entry.getFilename());
-                            sftpChannel.get(entry.getFilename(), dir.getAbsolutePath() + "\\" + entry.getFilename());
-                            sftpChannel.rm(entry.getFilename());
+                    for (ChannelSftp.LsEntry entry : entries) {
+                        if (entry.getAttrs().isDir() && entry.getFilename().equals(list.get("folderftp"))) {
+                            System.out.println((String) list.get("folderftp"));
+                            File dir = new File((String) list.get("path"));
+                            sftpChannel.cd((String) list.get("folderftp"));
+
+                            Vector<ChannelSftp.LsEntry> listFiles = sftpChannel.ls("*.zip");
+                            for (ChannelSftp.LsEntry entryFiles : listFiles) {
+                                if (!(dir).isDirectory())
+                                    dir.mkdir();
+                                System.out.println(entryFiles);
+                                sftpChannel.get(entryFiles.getFilename(), dir.getAbsolutePath() + "\\" + entryFiles.getFilename());
+                                sftpChannel.rm(entryFiles.getFilename());
+                            }
+                            listFiles = sftpChannel.ls("*.csv");
+                            for (ChannelSftp.LsEntry entryFiles : listFiles) {
+                                if (!(dir).isDirectory())
+                                    dir.mkdir();
+                                System.out.println(entryFiles);
+                                sftpChannel.get(entryFiles.getFilename(), dir.getAbsolutePath() + "\\" + entryFiles.getFilename());
+                                sftpChannel.rm(entryFiles.getFilename());
+                            }
                         }
-                        listFiles = sftpChannel.ls("*.csv");
-                        for (ChannelSftp.LsEntry entry : listFiles) {
-                            if (!(dir).isDirectory())
-                                dir.mkdir();
-                            System.out.println(dir.getAbsolutePath() + "\\" + entry.getFilename());
-                            sftpChannel.get(entry.getFilename(), dir.getAbsolutePath() + "\\" + entry.getFilename());
-                            sftpChannel.rm(entry.getFilename());
-                        }
-                        sftpChannel.exit();
-                        session.disconnect();
-                    } catch (JSchException | SftpException e) {
-                        e.printStackTrace();
+                        sftpChannel.cd("..");
                     }
                 }
             }
+        }
+            sftpChannel.exit();
+            session.disconnect();
+        } catch (JSchException | SftpException e) {
+            e.printStackTrace();
         }
     }
 }
